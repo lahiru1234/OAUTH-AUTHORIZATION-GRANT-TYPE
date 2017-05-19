db491ee958e0b5234a3e0d049c734cb828f35241
lahiru1234 <lahiruprabathbalasuriya.lpb@gmail.com>
2017-05-19 21:28:50 +0530

    Add files via upload

---
+++ new 2.php
@@ -0,0 +1,9 @@
+ Starting the Redirect 
+ 
+ <div class="panel panel-default">
+    <div class="panel-body">
+        Share your progress on Facebook!
+        <a href="{{ path('facebook_authorize_start') }}">Connect with Facebook</a>
+    </div>
+</div>
+---------------------------------------------------------------------------------
